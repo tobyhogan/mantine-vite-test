7cbccb868a1a1f3eb0a265eeb5ee1c0c439fda53
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-06-08 21:15:19 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 
+import Demo from "./Demo";
+
+
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}>
+    <Demo></Demo>
+    
+  </MantineProvider>;
 }
